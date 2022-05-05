0d7159c237ac00b2ae857ee1addfcd0f70a01fa8
Stephen Wade <stephen@stephenwade.me>
2022-05-04 22:44:35 -0400

    Database - Wrap up database queries

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,35 @@
+import type { Joke } from '@prisma/client';
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+
+import { db } from '~/utils/db.server';
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  if (!randomJoke) throw json('Not Found', { status: 404 });
+
+  const data: LoaderData = { joke: randomJoke };
+
+  return json(data);
+};
+
 export default function JokesIndexRoute() {
+  const data: LoaderData = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{data.joke.content}</p>
+      <Link to={data.joke.id}>"{data.joke.name}" Permalink</Link>
     </div>
   );
 }
