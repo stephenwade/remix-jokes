8e4ec3786e920b58d90dd8380adef6b505e16d43
Stephen Wade <stephen@stephenwade.me>
2022-06-19 19:50:05 -0400

    Authentication - Build logout action

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -4,27 +4,33 @@ import { Link, Outlet, useLoaderData } from '@remix-run/react';
 
 import stylesUrl from '~/styles/jokes.css';
 import { db } from '~/utils/db.server';
+import { getUser } from '~/utils/session.server';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
-type LoaderData = { jokes: { id: string; name: string }[] };
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+  jokes: { id: string; name: string }[];
+};
 
-export const loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    jokes: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: 'desc' },
-    }),
-  };
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokesPromise = db.joke.findMany({
+    take: 5,
+    select: { id: true, name: true },
+    orderBy: { createdAt: 'desc' },
+  });
+  const userPromise = getUser(request);
 
+  const [jokes, user] = await Promise.all([jokesPromise, userPromise]);
+
+  const data: LoaderData = { jokes, user };
   return json(data);
 };
 
 export default function JokesRoute() {
-  const data: LoaderData = useLoaderData<LoaderData>();
+  const data = useLoaderData<LoaderData>();
 
   return (
     <div className="jokes-layout">
@@ -36,6 +42,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
